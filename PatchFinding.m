dc051e9b5b817ce42fe5e3e1df5f4083b7285d37
Sahil Jain <jsahil730@gmail.com>
2020-05-31 15:36:33 +0530

    PatchFind Debugged

--- PatchFinding.m
+++ PatchFinding.m
@@ -1,16 +1,13 @@
 function Ans = PatchFinding(Images)
-K= size(Images, 4);  %number of frames 
-H= size(Images, 1);  %Height
-W= size(Images, 2);  %Width
-C= size(Images, 3);  %Channels
+[H,W,C,K] = size(Images);
 Fsel=5;              %number of matching patches selected per frame for ref patch
-Ans= double(zeros(H,W,C,K));
-Count= zeros(H,W,C,K);
+Ans= zeros(H,W,C,K);
+Count= zeros(H,W,C,K,'uint8');
 N= 8;                %size of ref patch
 G= 4;                %samlping interval
 for i=1:K
-    for ypos=1:4:H-G+1
-        for xpos=1:4:W-G+1
+    for ypos=1:G:H-N+1
+        for xpos=1:G:W-N+1
             for ch=1:C
                 RefP= Images(ypos: ypos+N-1, xpos: xpos+N-1, ch, i);
                 [Pjk, Pos]= Match(Images, RefP, ch, Fsel);
@@ -22,28 +19,24 @@ end
 end
 
 function [Pjk, Pos] = Match(Images, RefP, ch, Fsel)
-K= size(Images, 4);  %number of frames 
-H= size(Images, 1);  %Height
-W= size(Images, 2);  %Width
-C= size(Images, 3);  %Channels
+[~,~,~,K] = size(Images);
 N= size(RefP, 1);
 Pjk= zeros(N*N, Fsel*K);    %Pjk is the matrix of matched patches in the chanel ch
-Pos= zeros(Fsel*K, 3);      %Pos are the positions (Frame no, x, y) of those matched patches
+Pos= zeros(Fsel*K, 3,'uint8');      %Pos are the positions (Frame no, x, y) of those matched patches
 for i= 1:K
-    [P, Posf]= Select(Images(:,:, ch, i), Refp, Fsel);
-    Pjk(:, i*(Fsel-1)+1: i*Fsel)= P;
-    Pos(i*(Fsel-1)+1: i*Fsel, 2:3)= Posf;
-    Pos(i*(Fsel-1)+1: i*Fsel, 1)= K;
+    [P, Posf]= Select(Images(:,:, ch, i), RefP, Fsel);
+    Pjk(:, (i-1)*Fsel+1: i*Fsel)= P;
+    Pos((i-1)*Fsel+1: i*Fsel, 2:3)= Posf;
+    Pos((i-1)*Fsel+1: i*Fsel, 1)= i;
 end
 end
 
 function [P, Posf] = Select(Image, RefP, Fsel)
-H=size(Image, 1);
-W=size(Image, 2);
+[H,W]=size(Image);
 N= size(RefP, 1);
 P= zeros(N*N, Fsel);
 Ar= N*N*1000*ones(Fsel, 3);
-Posf= zeros(Fsel, 3);
+Posf= zeros(Fsel, 2,'uint8');
 for i=1: H-N+1
     for j=1:W-N+1
         currPatch= Image(i: i+N-1, j: j+N-1);
@@ -55,10 +48,9 @@ for i=1: H-N+1
         currVal(3)= j;
         for k=1:Fsel
             if Ar(k,1) > currVal(1)
-                temp= Ar(k);
-                Ar(k)= currVal;
+                temp= Ar(k,:);
+                Ar(k,:)= currVal;
                 currVal= temp;
-                
             end
         end
     end
@@ -68,7 +60,7 @@ for i=1: Fsel
     Posf(i,2)= Ar(i,3);
     currPatch= Image(Ar(i,2): Ar(i,2)+N-1, Ar(i,3): Ar(i,3)+N-1);
     curr= reshape(currPatch, N*N, 1);
-    P(i)= curr;
+    P(:,i)= curr;
 end
 end
 
--- test.m
+++ test.m
@@ -1,14 +1,13 @@
 clear; clc;
+addpath(genpath('mmread'));
 % I = imread('lena512.bmp');
 % 
 % I_noisy = imnoise(I,'salt & pepper',0.7);
 % 
-% filtI = Med_Filter(I_noisy,11);
+% [filtI,indices] = Med_Filter(I_noisy,11);
 % imshow(filtI);
 % figure;
 % imshow(I_noisy);
-% 
-% indices = (filtI == I_noisy)
 % 
 % psnr(I_noisy,I)
 % psnr(filtI,I)
@@ -20,4 +19,5 @@ vidframes_gray = zeros(H,W,1,F);
 for i = 1:F
     vidframes_gray(:,:,1,i) = rgb2gray(vidframes(:,:,:,i));
 end
-% PatchFinding(vidframes_gray);
+vidframes_gray = vidframes_gray/256;
+PatchFinding(vidframes_gray(:,:,:,1:20));
