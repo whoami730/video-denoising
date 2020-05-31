175c8b35017f64d446b7175ee30fb5c375faa1fb
Atulya Kumar Verma <45728654+atulyagh03@users.noreply.github.com>
2020-05-31 08:04:48 +0530

    Updated PatchFinding

--- PatchFinding.m
+++ PatchFinding.m
@@ -1,41 +1,49 @@
 function Ans = PatchFinding(Images)
-K= size(Images, 3);  %number of frames 
+K= size(Images, 4);  %number of frames 
 H= size(Images, 1);  %Height
 W= size(Images, 2);  %Width
+C= size(Images, 3);  %Channels
 Fsel=5;              %number of matching patches selected per frame for ref patch
-Ans= double(zeros(H,W,K));
-Count= zeros(H,W,K);
+Ans= double(zeros(H,W,C,K));
+Count= zeros(H,W,C,K);
 N= 8;                %size of ref patch
 G= 4;                %samlping interval
 for i=1:K
     for ypos=1:4:H-G+1
         for xpos=1:4:W-G+1
-            RefP= Image(i, ypos: ypos+N-1, xpos: xpos+N-1);
-            Pjk= Match(Images, RefP, Fsel);
+            for ch=1:C
+                RefP= Images(ypos: ypos+N-1, xpos: xpos+N-1, ch, i);
+                [Pjk, Pos]= Match(Images, RefP, ch, Fsel);
+            end
             
         end
     end
 end
 end
 
-function Pjk = Match(Images, RefP, Fsel)
-K= size(Images, 3);  %number of frames 
+function [Pjk, Pos] = Match(Images, RefP, ch, Fsel)
+K= size(Images, 4);  %number of frames 
 H= size(Images, 1);  %Height
 W= size(Images, 2);  %Width
+C= size(Images, 3);  %Channels
 N= size(RefP, 1);
-Pjk= zeros(N*N, Fsel*K);
+Pjk= zeros(N*N, Fsel*K);    %Pjk is the matrix of matched patches in the chanel ch
+Pos= zeros(Fsel*K, 3);      %Pos are the positions (Frame no, x, y) of those matched patches
 for i= 1:K
-    P= Select(Images(i), Refp, Fsel);
+    [P, Posf]= Select(Images(:,:, ch, i), Refp, Fsel);
     Pjk(:, i*(Fsel-1)+1: i*Fsel)= P;
+    Pos(i*(Fsel-1)+1: i*Fsel, 2:3)= Posf;
+    Pos(i*(Fsel-1)+1: i*Fsel, 1)= K;
 end
 end
 
-function P = Select(Image, RefP, Fsel)
+function [P, Posf] = Select(Image, RefP, Fsel)
 H=size(Image, 1);
 W=size(Image, 2);
 N= size(RefP, 1);
 P= zeros(N*N, Fsel);
 Ar= N*N*1000*ones(Fsel, 3);
+Posf= zeros(Fsel, 3);
 for i=1: H-N+1
     for j=1:W-N+1
         currPatch= Image(i: i+N-1, j: j+N-1);
@@ -55,8 +63,9 @@ for i=1: H-N+1
         end
     end
 end
-
 for i=1: Fsel
+    Posf(i,1)= Ar(i,2);
+    Posf(i,2)= Ar(i,3);
     currPatch= Image(Ar(i,2): Ar(i,2)+N-1, Ar(i,3): Ar(i,3)+N-1);
     curr= reshape(currPatch, N*N, 1);
     P(i)= curr;
